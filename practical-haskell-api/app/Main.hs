fc739927780eb3235c272bf5719fa0ec839f1d11
Vincent Jousse <vincent@jousse.org>
2017-12-05 09:06:05 +0100

    Add simple JSON api

--- practical-haskell-api/app/Main.hs
+++ practical-haskell-api/app/Main.hs
@@ -1,8 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Main where
 
 import qualified Web.Scotty as WS
 import Data.Monoid ((<>))
+import Data.Aeson (FromJSON, ToJSON)
+import GHC.Generics
+
+data User = User { userId :: Int, userName :: String } deriving (Show, Generic)
+
+instance ToJSON User
+instance FromJSON User
+
+bob :: User
+bob = User { userId = 1, userName = "bob" }
+
+jenny :: User
+jenny = User { userId = 2, userName = "jenny" }
+
+allUsers :: [User]
+allUsers = [bob, jenny]
+
+matchesId :: Int -> User -> Bool
+matchesId id user = userId user == id
 
 main :: IO ()
 main = do
@@ -12,3 +32,10 @@ main = do
         WS.get "/hello/:name" $ do
             name <- WS.param "name"
             WS.text ("hello " <> name <> "!")
+        WS.get "/users" $ do
+            WS.json allUsers
+        WS.get "/users/:id" $ do
+            id <- WS.param "id"
+            WS.json (filter (matchesId id) allUsers)
+
+
